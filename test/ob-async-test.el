d765a199614939dcb9df56151688f4f833b08c0c
Andrew Stahlman <astahlman@lyft.com>
2019-01-21 13:28:09 -0800

    Use callbacks instead of sleep in test cases
    Stop sleeping for a fixed amount of time in our test cases while we
    wait for results. Instead, add a macro that allows us to run code at
    two points in time:
    
    1. Immediately after executing `ctrl-c-ctrl-c`
    2. After the src-block is done executing

--- test/ob-async-test.el
+++ test/ob-async-test.el
@@ -1,5 +1,6 @@
 (require 'subr-x)
 (require 'cl-macs)
+(require 'cl-lib)
 
 (defun placeholder-p (s)
   "Return non-nil if S is a placeholder for an asynchronous result."
@@ -35,12 +36,31 @@ If NAME is non-nil, find the results block by name."
          (goto-char 0)
          ,@forms))))
 
-(defun wait-for-seconds (n)
-  "Sleep for N seconds.  This is a workaround for a bug in sleep-for.
-See http://stackoverflow.com/questions/14698081/elisp-sleep-for-doesnt-block-when-running-a-test-in-ert"
-  (let ((deadline (+ n (float-time))))
-    (while (< (float-time) deadline)
-      (sleep-for 1))))
+(defmacro ctrl-c-ctrl-c-with-callbacks (pre-callback pre-form post-callback post-form)
+  "Run `org-ctrl-c-ctrl-c' with the supplied :pre and :post callbacks.
+
+PRE-FORM is executed immediately after running `org-ctrl-c-ctrl-c'
+POST-FORM is executed after the src-block finishes execution"
+  (declare (debug (":pre" form ":post" form)))
+  (let ((finished (cl-gensym))
+        (sleep-interval-seconds (cl-gensym))
+        (timeout-seconds (cl-gensym))
+        (max-attempts (cl-gensym))
+        (num-attempts (cl-gensym)))
+    `(let* ((,finished nil)
+            (,sleep-interval-seconds .5)
+            (,timeout-seconds 10)
+            (,max-attempts (/ ,timeout-seconds ,sleep-interval-seconds))
+            (,num-attempts 0)
+            (org-babel-after-execute-hook (list (lambda ()
+                                                  ,post-form
+                                                  (setq ,finished t)))))
+       (org-ctrl-c-ctrl-c)
+       ,pre-form
+       (while (and
+               (not ,finished)
+               (<= (cl-incf ,num-attempts) ,max-attempts))
+         (sleep-for ,sleep-interval-seconds)))))
 
 (ert-deftest test-async-execute-fresh-sh-block ()
   "Test that we can insert results for a sh block that hasn't been executed yet"
@@ -51,10 +71,9 @@ See http://stackoverflow.com/questions/14698081/elisp-sleep-for-doesnt-block-whe
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait." (results-block-contents)))))))
 
 
 (ert-deftest test-async-ignore-lang-sh-block ()
@@ -79,16 +98,14 @@ See http://stackoverflow.com/questions/14698081/elisp-sleep-for-doesnt-block-whe
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents)))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait." (results-block-contents))))
       (goto-char 0)
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait." (results-block-contents)))))))
 
 (ert-deftest test-async-execute-python-block ()
   "Test that we can insert results for a sh block that hasn't been executed yet"
@@ -99,10 +116,9 @@ See http://stackoverflow.com/questions/14698081/elisp-sleep-for-doesnt-block-whe
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (= 2 (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (= 2 (results-block-contents)))))))
 
 (ert-deftest test-async-return-to-point-above-block ()
   "Test that results are inserted in the correct location
@@ -114,18 +130,31 @@ when content has been added above the source block"
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (re-search-backward "block:")
-      (end-of-line)
-      (newline-and-indent)
-      (insert "Here's some more stuff while we're waiting")
-      (let ((captured-point (point)))
-        (wait-for-seconds 5)
-        (should (= 2 (results-block-contents)))
-        (should (= captured-point (point)))
-        (should (re-search-backward "some more stuff"))
-        (should (re-search-forward "BEGIN_SRC python"))))))
+      (let (captured-point done-with-callback)
+        (ctrl-c-ctrl-c-with-callbacks
+         :pre (progn
+                (should (placeholder-p (results-block-contents)))
+                (re-search-backward "block:")
+                (end-of-line)
+                (newline-and-indent)
+                (insert "Here's some more stuff while we're waiting")
+                (setq captured-point (point)))
+         :post (progn
+                 (should (= 2 (results-block-contents)))
+                 (should (re-search-backward "some more stuff"))
+                 (should (re-search-forward "BEGIN_SRC python"))
+                 (setq done-with-callback t)))
+        ;; Our :post callback runs inside the context of
+        ;; `org-babel-execute-src-block:async'. We specifically want
+        ;; to check where (point) is when all our callbacks are
+        ;; finished, hence this sentinel
+        (let* ((sleep-interval-seconds .05)
+               (five-seconds 5)
+               (deadline (+ five-seconds (float-time))))
+          (while (and (not done-with-callback)
+                      (< (float-time) deadline))
+            (sleep-for sleep-interval-seconds)))
+        (should (= captured-point (point)))))))
 
 (ert-deftest test-async-return-to-point-below-block ()
   "Test that results are inserted in the correct location
@@ -137,19 +166,32 @@ when content has been added below the source block"
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (goto-char (point-max))
-      (newline-and-indent)
-      (insert "Here's some more stuff while we're waiting")
-      (let* ((captured-point (point))
-             (expected-point (- captured-point (- (length (ob-async--generate-uuid))
-                                                  (length "2")))))
-        (wait-for-seconds 5)
-        (should (= 2 (results-block-contents)))
-        (should (= expected-point (point)))
-        (should (re-search-backward "some more stuff"))
-        (should (re-search-backward "END_SRC"))))))
+      (let (captured-point done-with-callback)
+        (ctrl-c-ctrl-c-with-callbacks
+         :pre (progn
+                (should (placeholder-p (results-block-contents)))
+                (goto-char (point-max))
+                (newline-and-indent)
+                (insert "Here's some more stuff while we're waiting")
+                (setq captured-point (point)))
+         :post (progn
+                 (should (= 2 (results-block-contents)))
+                 (setq done-with-callback t)))
+        ;; Our :post callback runs inside the context of
+        ;; `org-babel-execute-src-block:async'. We specifically want
+        ;; to check where (point) is when all our callbacks are
+        ;; finished, hence this sentinel
+        (let* ((sleep-interval-seconds .05)
+               (five-seconds 5)
+               (deadline (+ five-seconds (float-time))))
+          (while (and (not done-with-callback)
+                      (< (float-time) deadline))
+            (sleep-for sleep-interval-seconds)))
+        (let ((expected-point (- captured-point (- (length (ob-async--generate-uuid))
+                                                   (length "2")))))
+          (should (= expected-point (point)))
+          (should (re-search-backward "some more stuff"))
+          (should (re-search-backward "END_SRC")))))))
 
 (ert-deftest test-async-execute-file-block ()
   "Test that we can insert results when header-arg :file is present"
@@ -160,12 +202,12 @@ when content has been added below the source block"
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "/tmp/foo" (results-block-contents)))
-      (let ((foo-contents (progn (find-file "/tmp/foo") (buffer-substring-no-properties (point-min) (point-max)))))
-        (should (string= "Don't wait on me\n" foo-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (progn
+               (should (string= "/tmp/foo" (results-block-contents)))
+               (let ((foo-contents (progn (find-file "/tmp/foo") (buffer-substring-no-properties (point-min) (point-max)))))
+                 (should (string= "Don't wait on me\n" foo-contents))))))))
 
 (ert-deftest test-async-execute-table-output ()
   "Test that we can insert table output"
@@ -178,12 +220,10 @@ for row in x:
 #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (message "Waiting")
-      (wait-for-seconds 8)
-      (should (equal '(("1,1" "1,2") ("2,1" "2,2")) (results-block-contents)))
-      (message "%s" (results-block-contents)))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (equal '(("1,1" "1,2") ("2,1" "2,2"))
+                            (results-block-contents)))))))
 
 (ert-deftest test-async-execute-tramp-block ()
   "Test that we can execute a block via Tramp with a :dir header-arg"
@@ -194,13 +234,13 @@ for row in x:
   #+END_SRC" user-login-name)))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= (format "%s /" user-login-name) (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= (format "%s /" user-login-name)
+                              (results-block-contents)))))))
 
 (ert-deftest test-async-ctrl-c-ctrl-c-hook ()
-  "Test that asynchronous execution works with org-ctrl-c-ctrl-c-hook."
+  "Test that asynchronous execution works with ctrl-c-ctrl-c-hook."
   (let ((buffer-contents "Here's a shell source block:
 
   #+BEGIN_SRC sh :async
@@ -209,10 +249,10 @@ for row in x:
         (org-ctrl-c-ctrl-c-hook '(ob-async-org-babel-execute-src-block)))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait."
+                              (results-block-contents)))))))
 
 (ert-deftest test-async-execute-named-block ()
   "Test that we can asynchronously execute a block when cursor is on the name."
@@ -223,10 +263,10 @@ for row in x:
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (re-search-forward "#\\+NAME")
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait."
+                              (results-block-contents)))))))
 
 (ert-deftest test-async-execute-named-block-with-results ()
   "Test that we can asynchronously execute a named block when results are anywhere in buffer."
@@ -239,19 +279,20 @@ for row in x:
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (re-search-forward "#\\+NAME")
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents)))
-      (let ((results-block-pos (save-excursion
-                                 (goto-char (point-min))
-                                 (re-search-forward "#\\+RESULTS")
-                                 (point-marker)))
-            (src-block-pos (save-excursion
-                             (goto-char (point-min))
-                             (re-search-forward "#\\+BEGIN_SRC")
-                             (point-marker))))
-        (should (< results-block-pos src-block-pos))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (progn
+               (should (string= "Sorry for the wait."
+                                (results-block-contents)))
+               (let ((results-block-pos (save-excursion
+                                          (goto-char (point-min))
+                                          (re-search-forward "#\\+RESULTS")
+                                          (point-marker)))
+                     (src-block-pos (save-excursion
+                                      (goto-char (point-min))
+                                      (re-search-forward "#\\+BEGIN_SRC")
+                                      (point-marker))))
+                 (should (< results-block-pos src-block-pos))))))))
 
 (ert-deftest test-async-execute-named-call-block ()
   "Test that we can asynchronously execute a named call block ."
@@ -267,23 +308,21 @@ for row in x:
   #+END_SRC "))
     (with-buffer-contents buffer-contents
       (re-search-forward "#\\+NAME: test")
-      (org-ctrl-c-ctrl-c)
-      (let ((results-block-pos (save-excursion
-                                 (org-babel-goto-named-result "test")
-                                 (point-marker)))
-            (src-block-pos (save-excursion
-                             (org-babel-goto-named-src-block "async-block")
-                             (point-marker))))
-        (should (< results-block-pos src-block-pos))
-        (goto-char results-block-pos)
-        (should (placeholder-p (results-block-contents "test")))
-        (wait-for-seconds 5)
-        (should (string= "Sorry for the wait."
-                         (results-block-contents "test")))))))
-
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents "test")))
+       :post (let ((results-block-pos (save-excursion
+                                        (org-babel-goto-named-result "test")
+                                        (point-marker)))
+                   (src-block-pos (save-excursion
+                                    (org-babel-goto-named-src-block "async-block")
+                                    (point-marker))))
+               (should (< results-block-pos src-block-pos))
+               (goto-char results-block-pos)
+               (should (string= "Sorry for the wait."
+                                (results-block-contents "test"))))))))
 
 (ert-deftest test-async-execute-silent-block ()
-  "Test that we can insert results for a sh block that hasn't been executed yet"
+  "Test that silent blocks should have no output"
   :expected-result :failed
   (let ((buffer-contents "Here's a sh source block:
 
@@ -292,10 +331,9 @@ for row in x:
   #+END_SRC"))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (not (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (not (results-block-contents)))))))
 
 (ert-deftest test-async-execute-call ()
   "Test that we can asynchronously execute a #+CALL element."
@@ -308,10 +346,10 @@ for row in x:
   #+CALL: async-block()"))
     (with-buffer-contents buffer-contents
       (re-search-forward "#\\+CALL")
-      (org-ctrl-c-ctrl-c)
-      (should (placeholder-p (results-block-contents)))
-      (wait-for-seconds 5)
-      (should (string= "Sorry for the wait." (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (string= "Sorry for the wait."
+                       (results-block-contents)))))))
 
 (ert-deftest test-confirm-evaluate ()
   "Test that we do not add a RESULTS block if evaluation is not confirmed"
@@ -336,9 +374,9 @@ for row in x:
                                                  (setq this-function-is-defined-in-a-hook #'+)))))
     (with-buffer-contents buffer-contents
       (org-babel-next-src-block)
-      (org-ctrl-c-ctrl-c)
-      (wait-for-seconds 5)
-      (should (= 2 (results-block-contents))))))
+      (ctrl-c-ctrl-c-with-callbacks
+       :pre (should (placeholder-p (results-block-contents)))
+       :post (should (= 2 (results-block-contents)))))))
 
 (ert-deftest test-output-to-file-with-dir ()
   "Test that :file paths are resolved referenced relative to :dir"
@@ -347,19 +385,17 @@ for row in x:
          (buffer-contents (format "
 #+BEGIN_SRC sh :dir /tmp :file %s :async
   echo %s
-#+END_SRC"
-                                  output-file
-                                  uuid)))
+#+END_SRC" output-file uuid)))
     (unwind-protect
         (progn
           (with-buffer-contents buffer-contents
             (org-babel-next-src-block)
-            (org-ctrl-c-ctrl-c)
-            (wait-for-seconds 5))
-          (let ((retrieved-value (with-temp-buffer
-                                   (insert-file-contents (expand-file-name output-file "/tmp"))
-                                   (string-trim (buffer-string)))))
-            (should (string= uuid retrieved-value))))
+            (ctrl-c-ctrl-c-with-callbacks
+             :pre (should (placeholder-p (results-block-contents)))
+             :post (let ((retrieved-value (with-temp-buffer
+                                            (insert-file-contents (expand-file-name output-file "/tmp"))
+                                            (string-trim (buffer-string)))))
+                     (should (string= uuid retrieved-value))))))
       ;; Clean up after ourselves
       (when (file-exists-p output-file)
         (delete-file output-file)))))
