d9431b24b6b0c4eb36fed38b3084719a6d821556
Stefan Kangas <stefan@marxist.se>
2022-01-02 19:06:38 +0100

    Add copyright and license to ob-async-test.el

--- ob-async.el
+++ ob-async.el
@@ -22,7 +22,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;; Package-Requires: ((async "1.9") (org "9.0.1") (emacs "24.4") (dash "2.14.1"))
 
